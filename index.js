ff8090c56e217da4dcc61c27aa6e1046476c21d4
Fernando R González <rumbaut2288@gmail.com>
2017-11-15 03:41:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n){
   var rankedList = document.querySelectorAll('.ranked-list');
   for (var i = 0; i < rankedList.length; i++) {
       for (var j = 0; j < rankedList[i].children.length; j++) {
-        rankedList[i].children[i]
+        rankedList[i].children[j].innerHTML;
       }
   }
 
